507c67e8c0eaa241af0b49b6d9e4c12a6752ce28
wooyong99 <wulovesk@naver.com>
2025-04-20 18:25:06 +0900

    refactor : api 경로 version 수정

--- src/main/java/com/pintoss/auth/module/user/api/AuthController.java
+++ src/main/java/com/pintoss/auth/module/user/api/AuthController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v2/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
