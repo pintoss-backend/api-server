c7d289caa553b8a10971f2bf10488ea0c6efcf05
r2dev12 <r2dev.12@imegagen.com>
2025-07-10 12:14:18 +0900

    feat : 요청 단위 MDC 설정을 위한 MdcLoggingFilter 구현 및 적용

---
+++ src/main/java/com/pintoss/auth/common/logging/MdcLoggingFilter.java
@@ -0,0 +1,29 @@
+package com.pintoss.auth.common.logging;
+
+import com.pintoss.auth.common.security.SecurityContextUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+
+public class MdcLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            MDC.put("requestId", UUID.randomUUID().toString());
+            MDC.put("userId", Optional.ofNullable(SecurityContextUtils.getUserId()).map(String::valueOf).orElse("anonymous"));
+            MDC.put("app_name", "pintoss-service");
+            MDC.put("env", "prod");
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
