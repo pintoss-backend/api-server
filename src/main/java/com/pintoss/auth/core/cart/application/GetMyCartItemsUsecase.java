4577ceb8221facabdab7ee152cb97fa5530dda7c
Koo <koosco136@gmail.com>
2025-07-14 03:39:08 +0900

    refactor(GetMyCartItemUseCase) : lombok 의존성 적용
    코드 가독성을 높이기 위해 Lombok 애노테이션 사용

--- src/main/java/com/pintoss/auth/core/cart/application/GetMyCartItemsUsecase.java
+++ src/main/java/com/pintoss/auth/core/cart/application/GetMyCartItemsUsecase.java
@@ -2,19 +2,17 @@ package com.pintoss.auth.core.cart.application;
 
 import com.pintoss.auth.api.security.SecurityContextUtils;
 import com.pintoss.auth.core.cart.application.dto.CartItemView;
-import com.pintoss.auth.core.cart.application.flow.viewer.CartItemProjectionReader;
-import java.util.List;
-
+import com.pintoss.auth.core.cart.application.flow.reader.CartItemProjectionReader;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class GetMyCartItemsUsecase {
 
     private final CartItemProjectionReader cartItemProjectionReader;
-
-    public GetMyCartItemsUsecase(CartItemProjectionReader cartItemProjectionReader) {
-        this.cartItemProjectionReader = cartItemProjectionReader;
-    }
 
     public List<CartItemView> getCartItems() {
         Long userId = SecurityContextUtils.getUserId();
