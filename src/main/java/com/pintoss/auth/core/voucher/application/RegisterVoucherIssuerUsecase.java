c215dcde1320b457554ba8abd42465dab808832d
Koo <koosco136@gmail.com>
2025-07-19 17:30:59 +0900

    fix : voucher usecase에서 도메인을 참조하도록 수정
    * usecase에서 엔티티 대신 도메인 참조
    * transaction을 usecase에서 처리

--- src/main/java/com/pintoss/auth/core/voucher/application/RegisterVoucherIssuerUsecase.java
+++ src/main/java/com/pintoss/auth/core/voucher/application/RegisterVoucherIssuerUsecase.java
@@ -1,19 +1,15 @@
 package com.pintoss.auth.core.voucher.application;
 
-import com.pintoss.auth.storage.voucher.jpa.entity.ContactInfoEmbeddable;
-import com.pintoss.auth.storage.voucher.jpa.entity.CsCenterEmbeddable;
-import com.pintoss.auth.storage.voucher.jpa.entity.DiscountEmbeddable;
-import com.pintoss.auth.storage.voucher.jpa.entity.HomePageEmbeddable;
-import com.pintoss.auth.core.voucher.domain.Voucher;
-import com.pintoss.auth.core.voucher.domain.VoucherIssuer;
 import com.pintoss.auth.core.voucher.application.dto.RegisterVoucherIssuerCommand;
+import com.pintoss.auth.core.voucher.application.flow.reader.VoucherIssuerValidator;
 import com.pintoss.auth.core.voucher.application.flow.writer.VoucherAdder;
 import com.pintoss.auth.core.voucher.application.flow.writer.VoucherIssuerAdder;
-import com.pintoss.auth.core.voucher.application.flow.validator.VoucherIssuerValidator;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.pintoss.auth.core.voucher.domain.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,33 +19,42 @@ public class RegisterVoucherIssuerUsecase {
     private final VoucherIssuerAdder voucherIssuerAdder;
     private final VoucherAdder voucherAdder;
 
+    @Transactional
     public void register(RegisterVoucherIssuerCommand command) {
-        voucherIssuerValidator.isIssuerNameDuplicated(command.getName());
+        voucherIssuerValidator.validateIssuerNameDuplicate(command.getName());
 
+        VoucherIssuer voucherIssuer = addIssuer(command);
+
+        addVouchers(command, voucherIssuer);
+    }
+
+    private VoucherIssuer addIssuer(RegisterVoucherIssuerCommand command) {
         VoucherIssuer voucherIssuer = VoucherIssuer.create(
-            command.getName(),
-            command.getCode(),
-            new DiscountEmbeddable(command.getCardDiscount(), command.getPhoneDiscount()),
-            new ContactInfoEmbeddable(
-                new HomePageEmbeddable(command.getHomePage()),
-                new CsCenterEmbeddable(command.getCsCenter())
-            ),
-            command.getDescription(),
-            command.getPublisher(),
-            command.getNote(),
-            command.getImageUrl(),
-            command.getFee()
+                command.getName(),
+                command.getCode(),
+                new Discount(command.getCardDiscount(), command.getPhoneDiscount()),
+                new ContactInfo(
+                        new HomePage(command.getHomePage()),
+                        new CsCenter(command.getCsCenter())
+                ),
+                command.getDescription(),
+                command.getPublisher(),
+                command.getNote(),
+                command.getImageUrl(),
+                command.getFee()
         );
         voucherIssuerAdder.add(voucherIssuer);
-
-        List<Voucher> vouchers = command.getVouchers().stream().map(v -> {
-            return Voucher.create(
-                voucherIssuer.getId(),
-                v.getName(),
-                voucherIssuer.getName(),
-                v.getPrice()
-            );
-        }).collect(Collectors.toList());
-        voucherAdder.add(vouchers);
+        return voucherIssuer;
     }
+
+    private void addVouchers(RegisterVoucherIssuerCommand command, VoucherIssuer voucherIssuer) {
+        List<Voucher> vouchers = command.getVouchers().stream().map(voucher -> Voucher.create(
+                voucherIssuer.getId(),
+                voucher.getName(),
+                voucherIssuer.getName(),
+                voucher.getPrice()
+        )).toList();
+        voucherAdder.addAll(vouchers);
+    }
+
 }
