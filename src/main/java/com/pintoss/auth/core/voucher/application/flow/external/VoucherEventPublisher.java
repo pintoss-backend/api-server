853589e6e062350cb8dd3a2f898db5438ff819f3
Koo <koosco136@gmail.com>
2025-07-20 18:19:09 +0900

    chore(VoucherEventPublisher) : 패키지 변경
    응집도를 높이기 위해 event 관련 모듈로 이동

--- src/main/java/com/pintoss/auth/core/voucher/application/flow/external/VoucherEventPublisher.java
+++ src/main/java/com/pintoss/auth/core/voucher/application/flow/external/VoucherEventPublisher.java
@@ -1,6 +1,6 @@
 package com.pintoss.auth.core.voucher.application.flow.external;
 
-import com.pintoss.auth.core.voucher.domain.VoucherPurchaseCompletedEvent;
+import com.pintoss.auth.support.event.voucher.VoucherPurchaseCompletedEvent;
 
 public interface VoucherEventPublisher {
     void publish(VoucherPurchaseCompletedEvent voucherPurchaseCompletedEvent);
