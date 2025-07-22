c85b058083623eacdcbeacba00840d3b3f9a2a03
Koo <koosco136@gmail.com>
2025-07-22 15:03:42 +0900

    fix(User) : 사용자 테이블명 수정

--- src/main/java/com/pintoss/auth/core/user/domain/User.java
+++ src/main/java/com/pintoss/auth/core/user/domain/User.java
@@ -1,26 +1,18 @@
 package com.pintoss.auth.core.user.domain;
 
-import com.pintoss.auth.support.exception.ErrorCode;
 import com.pintoss.auth.api.support.exception.client.BadRequestException;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.ConstraintMode;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import java.util.HashSet;
-import java.util.Set;
+import com.pintoss.auth.support.exception.ErrorCode;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
+@Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
